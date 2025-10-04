75e951fdc9323b2e00000c4d448c072135885ab9
David5p <k9glpuwd@students.codeinstitute.net>
2025-10-04 12:24:01 +0200

    readies code for deploy

--- tennis/settings.py
+++ tennis/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r2dp%4qvcddpjs3!ndeoan!xl34^h)%!&164bb3c%ezpa^0d0d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [,'.herokuapp.com']
 
 
 # Application definition
