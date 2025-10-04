5d208b4121d44a8b31e0a314450942594972b700
David5p <k9glpuwd@students.codeinstitute.net>
2025-10-04 15:47:46 +0200

    Create superuser

--- tennis/settings.py
+++ tennis/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r2dp%4qvcddpjs3!ndeoan!xl34^h)%!&164bb3c%ezpa^0d0d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
