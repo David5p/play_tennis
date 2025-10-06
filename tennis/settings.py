3e1aa9311c2c3f842bc9f31a1d0a61a7b3c49690
David5p <k9glpuwd@students.codeinstitute.net>
2025-10-06 09:07:15 +0200

    Update secret key

--- tennis/settings.py
+++ tennis/settings.py
@@ -33,8 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r2dp%4qvcddpjs3!ndeoan!xl34^h)%!&164bb3c%ezpa^0d0d'
-
+SECRET_KEY = os.environ.get("4f71e8bd52f8dbc06179fd08418ab6ea")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
