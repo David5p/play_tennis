5702e3f92b9b5ba361bd6d0a5b39694b3f06ba17
David5p <k9glpuwd@students.codeinstitute.net>
2025-10-07 13:44:51 +0200

    Create forms.py for user

---
+++ booking/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Booking
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['court', 'name', 'email', 'date', 'start_time', 'end_time']
+        widget = {
+            'date': forms.DateInput(attrs={'type': 'date'}), 
+            'start_time': forms.TimeInput(attrs={'type': 'time'}),
+            'end_time': forms.TimeInput(attrs={'type': 'time'}),
+        }
