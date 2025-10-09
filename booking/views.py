52ed7736aecbf8fc73380ab66f44b5b85c24b4d9
David5p <k9glpuwd@students.codeinstitute.net>
2025-10-09 10:14:39 +0200

    Srart creating user login structure

--- booking/views.py
+++ booking/views.py
@@ -16,4 +16,13 @@ def create_booking(request):
     
     return render(request, 'booking/create_booking.html',{'form': form})
 
-
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
