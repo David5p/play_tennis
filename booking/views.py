f9bc115c66cb18ca97f8db32a3da004a631e2d05
David5p <k9glpuwd@students.codeinstitute.net>
2025-11-06 13:06:19 +0100

    Fix register load page error

--- booking/views.py
+++ booking/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.utils import timezone
 from .models import Booking
 from .forms import BookingForm
+
 
 
 def home(request):
